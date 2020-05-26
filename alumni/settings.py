7fd4d017dae25c8091f52aeea390bfc6d09ae849
Aju Ajmal <dev@ajmalaju.com>
2020-05-26 22:48:00 +0530

    update

--- alumni/settings.py
+++ alumni/settings.py
@@ -207,5 +207,5 @@ MAX_UPLOAD_SIZE = "1048576"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
