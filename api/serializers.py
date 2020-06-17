4e595175afb8377b8c06694cccced3aed7a42f5a
Aju Ajmal <dev@ajmalaju.com>
2020-06-18 00:54:17 +0530

    update

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,56 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from account.models import Alumni,User,AlumniDB,CourseCompletion,AlumniProfile
+from account.choices import BRANCH , YEARSSTART, yearsend
+from account.utils import OptionalChoiceField
+from django.core.validators import RegexValidator
+import django.contrib.auth.password_validation as validators
+from django.core import exceptions
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class AlumniUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Alumni
+        fields = ('department', 'batch', 'contact')
+class CreateUserSerializer(serializers.HyperlinkedModelSerializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True,
                                      style={'input_type': 'password'})
+    email = serializers.EmailField()
+    alumni = AlumniUserSerializer(required=True)
+    def validate(self, data):
+        email = data['email']
+        username = data['username']
+        password = data.get('password')
+        errors = dict()
+        try:
+            validators.validate_password(password=password, user=User)
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError("Username already taken")
+        if errors:
+             raise serializers.ValidationError(errors)
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Email already exists")
+        return data
 
     class Meta:
-        model = get_user_model()
-        fields = ('username', 'password', 'first_name', 'last_name')
+        model = User
+        fields = ('username', 'password','email', 'first_name', 'last_name','alumni')
         write_only_fields = ('password')
         read_only_fields = ('is_staff', 'is_superuser', 'is_active',)
 
     def create(self, validated_data):
+        alumni_user = validated_data.pop('alumni')
         user = super(CreateUserSerializer, self).create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
+        Alumni.objects.create(user=user, **alumni_user)
         return user
+
+class ActivateAccount(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def save(self):
+        email = self.validated_data['email']
--- api/views.py
+++ api/views.py
@@ -5,8 +5,30 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.views import APIView
-from api.serializers import CreateUserSerializer
+from rest_framework import viewsets
 
+from api.serializers import CreateUserSerializer, ActivateAccount
+from account.models import Alumni,User,AlumniDB,CourseCompletion,AlumniProfile
+
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.sites.shortcuts import get_current_site
+from account.token_generator import account_activation_token
+from django.core.mail import EmailMessage
+
+def sendmail(request,user):
+    current_site = get_current_site(request)
+    email_subject = 'Activate Your Acc'
+    message = render_to_string('verify/mail/activate_account_mail.html', {
+    'user': user,
+    'domain': current_site.domain,
+    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+    'token': account_activation_token.make_token(user),
+    })
+    to_email = user.email
+    email = EmailMessage(email_subject, message, to=[to_email],from_email='alumni@cucek.in')
+    email.send()
 
 class CreateUserAPIView(CreateAPIView):
     serializer_class = CreateUserSerializer
@@ -34,3 +56,22 @@ class LogoutUserAPIView(APIView):
         # simply delete the token to force a login
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
+
+class ActivateAccount(CreateAPIView):
+    serializer_class =ActivateAccount
+
+    def post(self,request, *args, **kwargs):
+        serializer= self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            user = User.objects.get(email=serializer.data['email'])
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user == None :
+            pass
+        elif user.is_active == False :
+            sendmail(request,user)
+            print('send message')
+        print(request.data)
+        content = {'message': 'Hello, World!'}
+        return Response({**serializer.data, **content},status=status.HTTP_201_CREATED)
