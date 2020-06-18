96526ae9c952951ef5ea5219aec532f8b43c4c7b
Aju Ajmal <dev@ajmalaju.com>
2020-06-18 16:21:56 +0530

    update

--- api/serializers.py
+++ api/serializers.py
@@ -20,14 +20,15 @@ class AuthTokenSerializer(AuthTokenSerializer):
         if username and password:
             user = authenticate(request=self.context.get('request'),
                                 username=username, password=password)
-            if not user.is_active:
-                msg ={'notactivated':'Either activate or request for new account activation link.'}
-                raise serializers.ValidationError(msg, code='authorization')
+
             # The authenticate call simply returns None for is_active=False
             # users. (Assuming the default ModelBackend authentication
             # backend.)
             if not user:
                 msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+            if not user.is_active:
+                msg ={'notactivated':'Either activate or request for new account activation link.'}
                 raise serializers.ValidationError(msg, code='authorization')
         else:
             msg = _('Must include "username" and "password".')
