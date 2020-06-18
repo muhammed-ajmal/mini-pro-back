9221315010dd5ecff9e1ff4b735506f320f6cdc7
Aju Ajmal <dev@ajmalaju.com>
2020-06-18 17:18:29 +0530

    update

--- api/views.py
+++ api/views.py
@@ -72,7 +72,7 @@ class LogoutUserAPIView(APIView):
 
 class ActivateAccount(CreateAPIView):
     serializer_class =ActivateAccount
-
+    permission_classes = [AllowAny]
     def post(self,request, *args, **kwargs):
         serializer= self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
